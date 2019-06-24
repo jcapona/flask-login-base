c60bc70c026cca712950d66bbe283cb1113fba14
Jorge Capona <jcapona@gmail.com>
2019-06-24 12:08:57 -0400

    Update env var used for db connection

--- api/config.py
+++ api/config.py
@@ -4,7 +4,8 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY")
     LOG_FILE = os.environ.get("LOG_PATH")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
     TESTING = False
 
